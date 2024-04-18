ab3c2a299da72e2c1d07f73af55dd89cf4c7eaf6
Xozyain <akmd.uk@gmail.com>
2024-04-18 11:14:48 +0000

    parole perse

--- task3/check.sh
+++ task3/check.sh
@@ -4,5 +4,5 @@ occupied_mem=${occupied_mem:0:2}
 
 if (( $occupied_mem > 15 )); then
   echo "Less space available - currently you're using $occupied_mem% of disk space" | \
-  mail -s "Server memory status" akmd.uk@gmail.com -aFrom:yourpuccy@yandex.ru
+  mail -s "Server memory status" mail@gmail.com -aFrom:mail@yandex.ru
 fi
