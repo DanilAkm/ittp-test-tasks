facd5fd7c2aec301c814806e004220bbb5d2f767
Xozyain <akmd.uk@gmail.com>
2024-05-14 13:11:32 +0000

    tfm

--- task5/main.tf
+++ task5/main.tf
@@ -1,16 +1,38 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
+resource "yandex_compute_disk" "boot-disk" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = var.def_zone
+  size     = "20"
+  image_id = "fd8982cg2ridrq7erf1u"
+}
+
+resource "yandex_compute_instance" "default" {
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = var.def_zone
+
+  resources {
+    cores  = 2
+    memory = 2
   }
-  required_version = ">= 0.13"
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.boy.id
+  }
+
+  metadata = {
+    ssh-keys = "boy:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-provider "yandex" {
-  zone = "ru-central1-b"
-}
+resource "yandex_vpc_network" "boy" {}
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+resource "yandex_vpc_subnet" "boy" {
+  zone           = var.def_zone
+  network_id     = yandex_vpc_network.boy.id
+  v4_cidr_blocks = ["10.5.0.0/24"]
 }
---
+++ task5/variables.tf
@@ -0,0 +1,4 @@
+variable "def_zone" {
+  type        = string
+  description = "ru-central1-b"
+}
